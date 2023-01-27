f654be940cd35b34f738b458e0ad2bd3c381e3e5
E Luckie <eluckie.d@gmail.com>
2023-01-27 10:40:57 -0700

    pass Test2 for Pizza.prototype.chooseSize()

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,21 @@
-function Pizza(size, sauce, toppings) {
-  this.size = size;
-  this.sauce = sauce;
-  this.toppings = toppings;
+function Pizza() {
+  this.size = undefined;
+  this.sizePrice = 0;
+  this.sauce = undefined;
+  this.toppings = [];
 }
 
 Pizza.prototype.chooseSize = function(sizeChoice) {
   this.size = sizeChoice;
+  if (this.size === "small") {
+    this.sizePrice = "$10";
+  } else if (this.size === "medium") {
+    this.sizePrice = "$15";
+  } else if (this.size === "large") {
+    this.sizePrice = "$20";
+  } else if (this.size === "family") {
+    this.sizePrice = "$25";
+  }
 }
 
 Pizza.prototype.addSauce = function(sauceChoice) {
